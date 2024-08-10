d17d93dcbe2262401d319395338a96d3b896c786
Prabhjot Singh <prabhx.jott@gmail.com>
2024-08-10 14:33:58 +0530

    02JWT for week 3 assignment done

---
+++ Week 03 - JWT Auth and Middleware/02-jwt/index.js
@@ -0,0 +1,67 @@
+const jwt = require("jsonwebtoken");
+const jwtPassword = "secret";
+
+function isValidEmail(email) {
+  // Regular expression for basic email validation
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+}
+
+/**
+ * Generates a JWT for a given username and password.
+ *
+ * @param {string} username - The username to be included in the JWT payload.
+ *                            Must be a valid email address.
+ * @param {string} password - The password to be included in the JWT payload.
+ *                            Should meet the defined length requirement (e.g., 6 characters).
+ * @returns {string|null} A JWT string if the username and password are valid.
+ *                        Returns null if the username is not a valid email or
+ *                        the password does not meet the length requirement.
+ */
+function signJwt(username, password) {
+  if (!username || !password || password.length < 6 || !isValidEmail(username))
+    return null;
+  let token = jwt.sign(
+    {
+      username,
+      password,
+    },
+    jwtPassword
+  );
+  return token;
+}
+
+/**
+ * Verifies a JWT using a secret key.
+ *
+ * @param {string} token - The JWT string to verify.
+ * @returns {boolean} Returns true if the token is valid and verified using the secret key.
+ *                    Returns false if the token is invalid, expired, or not verified
+ *                    using the secret key.
+ */
+function verifyJwt(token) {
+  try {
+    return jwt.verify(token, jwtPassword) ? true : false;
+  } catch (e) {
+    return false;
+  }
+}
+
+/**
+ * Decodes a JWT to reveal its payload without verifying its authenticity.
+ *
+ * @param {string} token - The JWT string to decode.
+ * @returns {object|false} The decoded payload of the JWT if the token is a valid JWT format.
+ *                         Returns false if the token is not a valid JWT format.
+ */
+function decodeJwt(token) {
+  const decoded = jwt.decode(token);
+  return decoded && decoded.username && decoded.password ? true : false;
+}
+
+module.exports = {
+  signJwt,
+  verifyJwt,
+  decodeJwt,
+  jwtPassword,
+};
