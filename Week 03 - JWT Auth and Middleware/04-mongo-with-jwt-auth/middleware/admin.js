e83b735a52714ef626116d76a1ac7906ea62d13a
Prabhjot Singh <prabhx.jott@gmail.com>
2024-09-07 04:05:59 +0530

    added mongoDB api with JWT implementation

--- Week 03 - JWT Auth and Middleware/04-mongo-with-jwt-auth/middleware/admin.js
+++ Week 03 - JWT Auth and Middleware/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,12 @@
 // Middleware for handling auth
+
+const { verifyWebToken } = require('../utils');
+
 function adminMiddleware(req, res, next) {
-  // Implement admin auth logic
-  // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+  const token = req.headers.authorization.split(' ')[1];
+
+  if (verifyWebToken(token)) next();
+  else res.status(403).json({ msg: 'you are not authroised' });
 }
 
 module.exports = adminMiddleware;
--- Week 03 - JWT Auth and Middleware/04-mongo-with-jwt-auth/routes/admin.js
+++ Week 03 - JWT Auth and Middleware/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,14 +1,42 @@
 const { Router } = require('express');
 const adminMiddleware = require('../middleware/admin');
+const { Admin } = require('../db');
+const generateWebToken = require('../utils');
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-  // Implement admin signup logic
+router.post('/signup', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const existingUser = await Admin.findOne({ username });
+    if (existingUser) {
+      console.log(`Admin with name ${username} already exists`);
+      return res.status(409).json({ msg: 'Admin with the same name already exists' });
+    }
+    await Admin.create({
+      username,
+      password,
+    });
+    res.status(200).json({ msg: 'Admin created successfully' });
+  } catch (error) {
+    console.error('Error while signing up for admin: ', error);
+    res.status(500).json({ msg: 'Error while admin signing up' });
+  }
 });
 
-router.post('/signin', (req, res) => {
-  // Implement admin signup logic
+router.post('/signin', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const userMatched = await Admin.findOne({ username, password });
+    if (userMatched) {
+      const token = generateWebToken({ username });
+      return res.status(200).json({ msg: 'Admin with the same name already exists', token: token });
+    }
+    res.status(200).json({ msg: 'Either the username or password is wrong' });
+  } catch (error) {
+    console.error('Error while signing up for admin: ', error);
+    res.status(500).json({ msg: 'Error while admin signing up' });
+  }
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
